4bb27e8e2a9b029d41ceaa2b6c4f3553bf8c19f8
Omar Andres Martinez Rodriguez <omamarti@bancolombia.com.co>
2021-08-09 14:51:06 -0500

    Google sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,24 +3,25 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
-const login = async(req, res = response) =>{
+const login = async (req, res = response) => {
 
 
     const { correo, password } = req.body;
 
     try {
-        const usuario = await Usuario.findOne({correo});
+        const usuario = await Usuario.findOne({ correo });
 
         // Verificar si el email existe
-        if(!usuario){
+        if (!usuario) {
             return res.status(400).json({
                 msg: 'Usuario/password no son correctos - correo'
             })
         }
 
         // Verificar si el usuario esta activo en la bd
-        if(!usuario.estado){
+        if (!usuario.estado) {
             return res.status(400).json({
                 msg: 'Usuario/password no son correctos - estado:false'
             })
@@ -28,7 +29,7 @@ const login = async(req, res = response) =>{
 
         // verificar la contraseña
         const validarPassword = bcryptjs.compareSync(password, usuario.password);
-        if(!validarPassword){
+        if (!validarPassword) {
             return res.status(400).json({
                 msg: 'Usuario/password no son correctos - password'
             })
@@ -43,20 +44,68 @@ const login = async(req, res = response) =>{
             usuario,
             token
         })
-        
+
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            msg:'Hable con el administrador'
+            msg: 'Hable con el administrador'
         })
-        
+
+    }
+}
+
+const googleSigin = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const {correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+        if(!usuario){
+            // tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en BD tiene estado falso 
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usaurio bloqueado'
+            })
+        }
+
+        // Geenerar el json web tocken
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token de google no es reconocido'
+        })
+
     }
 
 
-   
 }
 
 
 module.exports = {
-    login
+    login,
+    googleSigin
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 
-const { login } = require('../controllers/auth');
+const { login, googleSigin } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
@@ -12,6 +12,11 @@ router.post('/login', [
     check('password','La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ] ,login );
+
+router.post('/google', [
+    check('id_token', 'El id_token es obligatorio').not().isEmpty(),
+    validarCampos
+] ,googleSigin );
 
 
 
